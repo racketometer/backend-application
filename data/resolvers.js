cdf660dc71414687212bbe8bcace1f1bd1506bb9
Jesper O. Christensen <karnich99@gmail.com>
2016-08-30 22:09:40 +0200

    function replaced with ES6 syntax mutation now returns user

--- data/resolvers.js
+++ data/resolvers.js
@@ -9,10 +9,12 @@ const resolvers = {
   Mutation: {
     createUser: (root, { email, pw }) => {
       return Users.findOne({ "email": email, "password": pw }).then( (user) => {
-        if (user != null) return null
+        if (!user) return null
 
-        return Users.create({ email: email, password: pw, joinedAt: new Date().toString(), age: 2, role: "boho" }, function(err, user) {
+        return Users.create({ email: email, password: pw, joinedAt: new Date().toString(), age: 2, role: "boho" }, (err, user) => {
           if(err) return err;
+
+          return user;
         })
       });
     }
