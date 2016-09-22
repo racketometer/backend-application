d6c4e6f977f76b15a329208f2be22123d7f6c176
Jesper O. Christensen <karnich99@gmail.com>
2016-09-22 13:37:42 +0200

    mongoose models for measurement and user

--- data/connectors.js
+++ data/connectors.js
@@ -1,21 +1,44 @@
 import Mongoose from "mongoose";
 
-const mongo = Mongoose.connect('mongodb://localhost:27017/test', (err) => {
+const mongo = Mongoose.connect("mongodb://localhost:27017/test", (err) => {
   if(err){
-    console.error('Could not connect to MongoDB on port 27017');
+    console.error("Could not connect to MongoDB on port 27017");
   }
 });
 
 const UserSchema = Mongoose.Schema({
-  username: String,
-  password: String,
+  displayName: String,
   email: String,
-  role: String,
-  age: Number,
-  joinedAt: String,
+  password: String,
+  firstName: String,
+  lastName: String,
+  isConsultant: Boolean,  
+  isCoach: Boolean,
+  birthday: Date,
+  startedPlaying: Date,
+  coaches: [{ _id, name }],
+  friends: [{ _id, name }],
+  timestamps: true
 })
 
-const Users = Mongoose.model('users', UserSchema);
+const MeasurementSchema = Mongoose.Schema({
+  date: Date,
+  uploadedBy: Schema.Types.ObjectId,
+  data: [Number],
+  strokes: Number,
+  strokeType: [String],
+  maxRacketSpeed: Number,
+  maxShuttlecockSpeed: Number, 
+  sensorNo: String,
+  racketType: String,
+  algorithmVersion: String,
+  user_id: Schema.Types.ObjectId,
+  timestamps: true
+})
+
+const Users = Mongoose.model("user", UserSchema);
+
+const Measurements = Mongoose.model("measurement", MeasurementSchema);
 
 // export models
-export { Users };
+export { Users, Measurements };
