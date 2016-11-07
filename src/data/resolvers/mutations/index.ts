48e5c313fd7bb8059d90a7a7b12b3fb042221f9c
Jesper O. Christensen <karnich99@gmail.com>
2016-11-07 10:24:41 +0100

    logout was causing issue inside of query. Moved to mutations

--- src/data/resolvers/mutations/index.ts
+++ src/data/resolvers/mutations/index.ts
@@ -2,18 +2,21 @@ export * from "./changePassword";
 export * from "./createAutoUser";
 export * from "./createUser";
 export * from "./viewer";
+export * from "./logout";
 
 import {
   changePassword,
   createAutoUser,
   createUser,
   viewer,
+  logout,
 } from "./";
 
 export const MutationViewer = {
   changePassword,
   createAutoUser,
   createUser,
+  logout,
 };
 
 export const Mutation = {
