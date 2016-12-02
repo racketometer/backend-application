8ceb4102308dfdbd7a92d8f494803a1e816b2c64
Jesper O. Christensen <karnich99@gmail.com>
2016-12-02 09:39:12 +0100

    removed unnecessary code

--- src/data/connectors/index.ts
+++ src/data/connectors/index.ts
@@ -18,7 +18,7 @@ let ROM_DB_URL;
 
 if (process.env.NODE_ENV === "prod") {
   ROM_DB_URL = process.env.ROM_DB_PROD;
-} else if (process.env.NODE_ENV === "test") {
+} else {
   ROM_DB_URL = process.env.ROM_DB_TEST;
 }
 
@@ -27,7 +27,7 @@ export const MongooseConnection = Mongoose.connect(ROM_DB_URL)
     console.error("Could not connect to MongoDB on mlab", connectError);
   })
   .then(() => {
-    if (process.env.NODE_ENV === "test" && process.env.NODE_ENV !== "prod") {
+    if (process.env.NODE_ENV === "test") {
       seeder.seed(seedData, { dropCollections: false });
     }
   })
